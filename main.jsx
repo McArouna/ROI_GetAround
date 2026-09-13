d6fa3c164f0733fa62f85666d715a3b3c99b66d0
McArouna <hmdabadou@gmail.com>
2026-09-13 19:51:48 +0100

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import ROITool from './ROITool.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ROITool />
-  </React.StrictMode>,
-)
